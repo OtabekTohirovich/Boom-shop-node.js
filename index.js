5303958125da45e93ce54b22052834efeb3b1243
OtabekTohirovich <tohirovo630@gmail.com>
2023-03-07 13:35:10 +0500

    Progress Node.js shop-app

---
+++ index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import path, {dirname} from 'path'
+import { fileURLToPath } from "url";
+import { create } from "express-handlebars";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express();
+
+const hbs = create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+
+
+
+
+const PORT = process.env.PORT || 4100;
+app.listen(PORT, () => {
+  console.log("server is running port");
+});
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req, res)=>{
+//     res.status(200)
+//     res.sendFile(path.join(__dirname, "views", 'index.html'))
+//     // res.send('Main page')
+// })
+
+// app.get('/about', (req, res)=>{
+//     res.status(200)
+//     res.sendFile(path.join(__dirname, "views", 'about.html'))
+//     // res.send('about page')
+// })
